dbb329df9fb9e822180b0fbdebf431ef20c0df4c
Marcin Bednarz <marcinbednarz102@outlook.com>
2023-01-11 16:49:58 +0100

    == bug fixed

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -4,9 +4,9 @@ import { connect } from "react-redux";
 function FeatureRequest(props: any) {
   return (
     <>
-      {props.requestStage == "main" && <p>main</p>}
-      {props.requestStage == "description" && <p>description</p>}
-      {props.requestStage == "customer" && <p>customer</p>}
+      {props.requestStage === "main" && <p>main</p>}
+      {props.requestStage === "description" && <p>description</p>}
+      {props.requestStage === "customer" && <p>customer</p>}
     </>
   );
 }
