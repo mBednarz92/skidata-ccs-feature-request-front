d7d687b149739ae8b828e3c7b057500d420d0542
Marcin Bednarz <marcinbednarz102@outlook.com>
2023-01-25 10:50:18 +0100

    sign in view

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,54 @@ import FeatureRequest from "./components/pages/index";
 import styles from "./App.module.scss";
 
 import { Login } from "@microsoft/mgt-react";
+import { Providers, ProviderState } from "@microsoft/mgt-element";
+
+import SkidataLogo from "./assets/skidata-logo.png";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    };
+  }, []);
+
   return (
     <div className={styles.app}>
       <header>
         <Login />
       </header>
+
+      {isSignedIn ? (
+        <FeatureRequest />
+      ) : (
+        <div className={styles.welcome__container}>
+          <div className={styles.welcome__box}>
+            <h1 className={styles.welcome__header}>Welcome to</h1>
+            <div className={styles.logo__container}>
+              <img
+                alt="skidata_logo"
+                src={SkidataLogo}
+                className={styles.welcomeImage}
+              />
+              <h1 className={styles.logo__container__title}>Feature Request</h1>
+            </div>
+            <Login />
+            <p>Please sign in with Skidata MS account to create request.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
